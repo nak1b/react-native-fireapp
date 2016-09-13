021e5028457b5a72f23cdbf17f0dc6d3efbc6a60
Nakib <nbmomin14@gmail.com>
2016-09-13 09:58:32 -0700

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+
+class fireApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+AppRegistry.registerComponent('fireApp', () => fireApp);
