e2cde13fb5a4e9a7bb104233866b17cf29f51c59
Nakib <nbmomin14@gmail.com>
2016-09-13 10:21:55 -0700

    Added editor config

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyAbeQdYYB9mBpW2eNJRvxl4R7CxZBRxHAw",
+  authDomain: "fireapp-4c333.firebaseapp.com",
+  databaseURL: "https://fireapp-4c333.firebaseio.com",
+  storageBucket: "",
+};
+
+firebase.initializeApp(config);
+
+
 
 class fireApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        
+
       </View>
     );
   }
